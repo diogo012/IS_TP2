3e6910743143d3b6eba50ef1a8284b63dbe24fc0
luisteofilo <luisfgteofilo@gmail.com>
2023-12-11 13:32:43 +0000

    Change Entities app to Next.js

--- src/frontend/ent/app/players/page.js
+++ src/frontend/ent/app/players/page.js
@@ -1,4 +1,5 @@
-import {useEffect, useState} from "react";
+"use client";
+import {useCallback, useEffect, useState} from "react";
 import {
     CircularProgress,
     Pagination,
@@ -10,6 +11,7 @@ import {
     TableHead,
     TableRow
 } from "@mui/material";
+import {useSearchParams, useRouter, usePathname} from 'next/navigation';
 
 const DEMO_PLAYERS = [
     {"id": "31", "name": "Alexis Sanchez", "age": "33"},
@@ -45,31 +47,44 @@ const DEMO_PLAYERS = [
 ];
 
 
-function Players() {
+export default function PlayersPage({pagea}) {
 
-    const PAGE_SIZE = 10;
-    const [page, setPage] = useState(1);
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const router = useRouter();
+    const createQueryString = useCallback(
+        (name, value) => {
+            const params = new URLSearchParams(searchParams)
+            params.set(name, value)
+
+            return params.toString()
+        },
+        [searchParams]
+    );
     const [data, setData] = useState(null);
     const [maxDataSize, setMaxDataSize] = useState(DEMO_PLAYERS.length);
+
+    const page = parseInt(searchParams.get('page')) || 1;
+    const PAGE_SIZE = 10;
 
     useEffect(() => {
         //!FIXME: this is to simulate how to retrieve data from the server
         //!FIXME: the entities server URL is available on process.env.REACT_APP_API_ENTITIES_URL
         setData(null);
         setTimeout(() => {
-            console.log(`fetching from ${process.env.REACT_APP_API_ENTITIES_URL}`)
+            console.log(`fetching from ${process.env.NEXT_PUBLIC_API_ENTITIES_URL}`)
             setData(DEMO_PLAYERS.filter((item, index) => Math.floor(index / PAGE_SIZE) === (page - 1)));
         }, 500);
     }, [page])
 
     return (
         <>
-            <h1>Players</h1>
+            <h1 sx={{fontSize: "100px"}}>Players</h1>
 
             <TableContainer component={Paper}>
                 <Table sx={{minWidth: 650}} aria-label="simple table">
                     <TableHead>
-                        <TableRow>
+                        <TableRow sx={{backgroundColor: "lightgray"}}>
                             <TableCell component="th" width={"1px"} align="center">ID</TableCell>
                             <TableCell>Player Name</TableCell>
                             <TableCell align="center">Age</TableCell>
@@ -81,7 +96,6 @@ function Players() {
                                 data.map((row) => (
                                     <TableRow
                                         key={row.id}
-                                        style={{background: "gray", color: "black"}}
                                     >
                                         <TableCell component="td" align="center">{row.id}</TableCell>
                                         <TableCell component="td" scope="row">
@@ -103,22 +117,18 @@ function Players() {
                 </Table>
             </TableContainer>
             {
-                maxDataSize && <div style={{background: "black", padding: "1rem"}}>
-                    <Pagination style={{color: "black"}}
-                                variant="outlined" shape="rounded"
-                                color={"primary"}
-                                onChange={(e, v) => {
-                                    setPage(v)
-                                }}
-                                page={page}
-                                count={Math.ceil(maxDataSize / PAGE_SIZE)}
-                    />
-                </div>
+                maxDataSize && <Pagination style={{color: "black", marginTop: 8}}
+                                           variant="outlined" shape="rounded"
+                                           color={"primary"}
+                                           onChange={(e, v) => {
+                                               router.push(pathname + '?' + createQueryString('page', v))
+                                           }}
+                                           page={page}
+                                           count={Math.ceil(maxDataSize / PAGE_SIZE)}
+                />
             }
 
 
         </>
     );
 }
-
-export default Players;
